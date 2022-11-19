a27d51a0db60ec9210fa386e14ecf22bcc589540
vyvuluan <vyvuluan@gmail.com>
2022-11-20 00:14:00 +0700

    3

--- app/Http/Controllers/admin/ManageBaoCaoController.php
+++ app/Http/Controllers/admin/ManageBaoCaoController.php
@@ -37,19 +37,13 @@ class ManageBaoCaoController extends Controller
                 //     ->where('status', 4)
                 //     ->first();
                 $total_px[$stringMY] = DB::select(
-                    'select sum("tongTien") as tongTien from phieu_xuats where "created_at" >= :from and "created_at"  < :to and status = 4 limit 1',
-                    [
-                        'from' => $from,
-                        'to' => $to
-                    ]
+                    'select sum("tongTien") as tongTien from phieu_xuats where "created_at" >= ? and "created_at"  < ?  limit 1',
+                    [$from, $to]
                 );
 
                 $total_pn[$stringMY] = DB::select(
-                    'select sum("tongTien") as tongTien from phieu_nhaps where "created_at" >= :from and "created_at" and < :to and status = 1 limit 1',
-                    [
-                        'from' => $from,
-                        'to' => $to
-                    ]
+                    'select sum("tongTien") as tongTien from phieu_nhaps where "created_at" >= ? and "created_at" and < ? limit 1',
+                    [$from, $to]
                 );
             } else {
                 $timeNow = $timeNow->subMonth();
@@ -71,19 +65,13 @@ class ManageBaoCaoController extends Controller
                 //     ->first();
 
                 $total_px[$stringMY] = DB::select(
-                    'select sum("tongTien") as tongTien from phieu_xuats where "created_at" >= :from and "created_at"  < :to and status = 4 limit 1',
-                    [
-                        'from' => $from,
-                        'to' => $to
-                    ]
+                    'select sum("tongTien") as tongTien from phieu_xuats where "created_at" >= ? and "created_at"  < ?  limit 1',
+                    [$from, $to]
                 );
 
                 $total_pn[$stringMY] = DB::select(
-                    'select sum("tongTien") as tongTien from phieu_nhaps where "created_at" >= :from and "created_at" and < :to and status = 1 limit 1',
-                    [
-                        'from' => $from,
-                        'to' => $to
-                    ]
+                    'select sum("tongTien") as tongTien from phieu_nhaps where "created_at" >= ? and "created_at" and < ? limit 1',
+                    [$from, $to]
                 );
             }
         }
